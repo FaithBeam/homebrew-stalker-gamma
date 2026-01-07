14ecbb5caf7f37354601259456e69f4822d67b26
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2026-01-07 16:56:02 -0500

    Update stalker-gamma to version 0.4.0

--- Casks/stalker-gamma.rb
+++ Casks/stalker-gamma.rb
@@ -1,14 +1,13 @@
 cask "stalker-gamma" do
-  version "0.3.2"
-  sha256 "b4dbb8906d0095c8b5f3c6ff170fb582ce28cb932fa7ecfcf87e65044003c0fe"
+  version "0.4.0"
+  sha256 "9182b0570ff70441fa66fe3fa57355e3754ec6048c815242164ba1c2d3c545cd"
 
-  url "https://github.com/FaithBeam/stalker-gamma-cli/releases/download/#{version}/stalker-gamma.#{version}+mac.arm64.zip"
+  url "https://github.com/FaithBeam/stalker-gamma-cli/releases/download/#{version}/stalker-gamma.#{version}+mac.arm64.tar.gz"
   name "stalker-gamma"
   desc "Install Stalker GAMMA via CLI"
   homepage "https://github.com/FaithBeam/stalker-gamma-cli"
 
   depends_on arch: :arm64
-  depends_on formula: "git"
   depends_on formula: "libidn2"
   depends_on formula: "zstd"
 
