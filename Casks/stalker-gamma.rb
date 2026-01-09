e88b08761cefa59faec81f438ec173b31ae22e4a
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2026-01-09 06:18:46 -0500

    Update stalker-gamma to version 0.7.0

--- Casks/stalker-gamma.rb
+++ Casks/stalker-gamma.rb
@@ -1,6 +1,6 @@
 cask "stalker-gamma" do
-  version "0.6.0"
-  sha256 "c0e01a7d5499f2434c29d635f0d2a376687fa60c61ff97efcfd453b5bc5a7c1a"
+  version "0.7.0"
+  sha256 "c9722aeb5276e2a30f0ac66508c046372877c25cf573cbca83232991b4bcb53c"
 
   url "https://github.com/FaithBeam/stalker-gamma-cli/releases/download/#{version}/stalker-gamma.#{version}+mac.arm64.tar.gz"
   name "stalker-gamma"
