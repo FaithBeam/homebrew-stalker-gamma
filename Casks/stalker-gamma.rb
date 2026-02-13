684c1fc53c0553379772bbf5cce67a29ecd478df
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2026-02-13 06:45:05 -0500

    Update stalker-gamma to version 1.9.1

--- Casks/stalker-gamma.rb
+++ Casks/stalker-gamma.rb
@@ -1,13 +1,19 @@
 cask "stalker-gamma" do
-  version "1.8.3"
-  sha256 "a454c2c4f7685b0aba99a4a5201f45de629f45cef8177666185c82dc1f84ad2b"
+  arch arm: "arm64", intel: "x64"
 
-  url "https://github.com/FaithBeam/stalker-gamma-cli/releases/download/#{version}/stalker-gamma+mac.arm64.tar.gz"
+  version "1.9.1"
+
+  if Hardware::CPU.arm?
+    sha256 "43f9c11d8ee668af3e38b2d2fe67cf19fc67e59f5a84332b24c15c7767240ec4"
+  else
+    sha256 "c90f8e2bdbe4a5d854b03909b3d6c4f36bcfbb65ed98baaef4fae9447a8b2e76"
+  end
+
+  url "https://github.com/FaithBeam/stalker-gamma-cli/releases/download/#{version}/stalker-gamma+mac.#{arch}.tar.gz"
   name "stalker-gamma"
   desc "Install Stalker GAMMA via CLI"
   homepage "https://github.com/FaithBeam/stalker-gamma-cli"
 
-  depends_on arch: :arm64
   depends_on formula: "libidn2"
   depends_on formula: "zstd"
 
